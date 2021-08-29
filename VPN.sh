b9c5e6f559f5e46bd229d5157e851aac5f3c17a8
胡锦浩 <jinhao0113@outlook.com>
2021-08-29 09:57:00 +0800

    Create VPN.sh

---
+++ VPN.sh
@@ -0,0 +1,4 @@
+echo '--------------------------------------------------------------'
+curl https://gist.github.com/minacle/e9dedb8c17025a23a453f8f30eced3da/raw/908b944b3fe2e9f348fbe8b8800daebd87b5966c/openssl@1.0.rb
+echo '--------------------------------------------------------------'
+curl https://gist.github.com/minacle/e9dedb8c17025a23a453f8f30eced3da/raw/908b944b3fe2e9f348fbe8b8800daebd87b5966c/chntpw.rb
